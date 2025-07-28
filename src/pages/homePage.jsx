b5c3fe5203c2e8a7e2b562a03eb3c61e5e4fff9d
veikkapuhalainen <veikka.puhalainen@aalto.fi>
2025-07-28 15:53:31 +0300

    Preparation for the project

---
+++ src/pages/homePage.jsx
@@ -0,0 +1,10 @@
+function HomePage () {
+    return (
+        <div>
+            <h1>Oikotie-Haku</h1>
+            <a href="https://asunnot.oikotie.fi/myytavat-asunnot">Oikotie myytävät asunnot</a>
+        </div>
+    )
+}
+
+export default HomePage;
