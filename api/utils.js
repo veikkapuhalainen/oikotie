05b8f996ccc5f2fcbc26c24f7fe2f0aa9795955f
veikkapuhalainen <veikka.puhalainen@aalto.fi>
2025-08-24 19:39:58 +0300

    Removed old logic to make utils.js more clear

--- api/utils.js
+++ api/utils.js
@@ -1,11 +1,11 @@
 const LOCATIONS_HELSINKI = JSON.stringify([[64, 6, 'Helsinki']]);
 
-const API_URL = 'https://asunnot.oikotie.fi/api/cards';
 const HTML_URL = 'https://asunnot.oikotie.fi/myytavat-asunnot';
 
+/*
 const PARAMS = {
   cardType: 100,
-  limit: 5000,
+  limit: 1000,
   offset: 0,
   locations: LOCATIONS_HELSINKI,
   'constructionYear[max]': 2025,
@@ -16,6 +16,7 @@ const PARAMS = {
   'size[max]': 10000,
   sortBy: 'published_sort_desc'
 };
+*/
 
 export async function getHeaders() {
   const response = await fetch(HTML_URL);
@@ -30,54 +31,4 @@ export async function getHeaders() {
     'OTA-loaded': loaded,
     'OTA-token': token
   };
-}
-
-export async function fetchFromOikotie() {
-  const headers = await getHeaders();
-  const url = new URL(API_URL);
-
-  for (const key in PARAMS) {
-    if (Array.isArray(PARAMS[key])) {
-      PARAMS[key].forEach(val => url.searchParams.append(key, val));
-    } else {
-      url.searchParams.append(key, PARAMS[key]);
-    }
-  }
-
-  const res = await fetch(url.href, { headers });
-  const json = await res.json();
-
-  if (!json.cards) {
-    console.log(`❌ Fetching apartments failed from Oikotie.`);
-    return [];
-  } else {
-    return json.cards.map(card => {
-        const rawPrice = card.price;
-        const size = card.size;
-        const numericPrice = parseFloat((rawPrice || '').replace(/[^\d,.]/g, '').replace(',', '.'));
-        const pricePerSqm = numericPrice && size ? Math.round(numericPrice / size) : null;
-
-        return {
-            id: card.id,
-            url: card.url,
-            description: card.description,
-            roomConfiguration: card.roomConfiguration,
-            rooms: card.rooms,
-            published: card.published,
-            size: card.size,
-            price: card.price,
-            pricePerSqm,
-            address: card.buildingData?.address,
-            district: card.buildingData?.district,
-            city: card.buildingData?.city,
-            year: card.buildingData?.year,
-            buildingType: card.buildingData?.buildingType,
-            brand: card.brand?.name,
-            visits: card.visits,
-            visitsWeekly: card.visits_weekly,
-            location: card.location,
-            image: card.images?.wide
-        };
-    });
-  }
 }
